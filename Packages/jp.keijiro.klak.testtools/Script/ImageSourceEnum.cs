aaa10631e2d600e0ef99d2e3bda2367a3b4d41a1
Keijiro Takahashi <keijiro@gmail.com>
2024-02-26 20:37:35 +0900

    Quick fixes for the last changes

--- Packages/jp.keijiro.klak.testtools/Script/ImageSourceEnum.cs
+++ Packages/jp.keijiro.klak.testtools/Script/ImageSourceEnum.cs
@@ -5,14 +5,14 @@ namespace Klak.TestTools {
 public enum ImageSourceType
 {
     Texture,
-    [InspectorName("Image URL")] TextureUrl,
     Video,
-    [InspectorName("Video URL")] VideoUrl,
     Webcam,
     Card,
     Gradient,
     Camera,
-    [InspectorName("NDI")] Ndi
+    [InspectorName("NDI")] Ndi,
+    [InspectorName("Image URL")] TextureUrl,
+    [InspectorName("Video URL")] VideoUrl
 }
 
 } // namespace Klak.TestTools
